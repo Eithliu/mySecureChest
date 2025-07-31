1de2d6f181af97cacab7699af7650381ad8f90db
Eithliu <iris.benoit@gmail.com>
2025-07-31 22:54:44 +0200

    complexify the randomization of the password

---
+++ src/components/GeneratorSection.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+  isGenerator: boolean,
+  length: string,
+}>();
+
+const numberChosen = ref(false);
+const specialCharsChosen = ref(false);
+const pickedLength = ref('');
+
+function randomisePassword(length: string): string {
+  let result = []
+  let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let numbers = '0123456789';
+  let specialChars = 'éèçà!è§(&@-_)#@êëù%$*€`£=+/:.;?,âäûüîïôö'
+  for (let i = 0; i < +length; i++) {
+    if (numberChosen.value) {
+      const setOfChars = chars + numbers;
+      result.push(setOfChars.charAt(Math.floor(Math.random() * +length)));
+    } else if (specialCharsChosen.value) {
+      const setOfChars = chars + specialChars;
+      result.push(setOfChars.charAt(Math.floor(Math.random() * +length)));
+    } else if (numberChosen && specialCharsChosen) {
+      const setOfChars = chars + numbers + specialChars;
+      result.push(setOfChars.charAt(Math.floor(Math.random() * +length)));
+    }
+    console.log(result);
+  }
+    return result.join('');
+}
+
+function displayGeneratedPassword(length: string) {
+  return randomisePassword(length);
+}
+</script>
+
+<template>
+  <div class="what-to-include">
+    <p>Include</p>
+    <div class="can-include">
+      <label for="characters">characters</label>
+      <input type="checkbox" value="Characters" id="characters" checked="">
+      <label for="numbers">numbers</label>
+      <input type="checkbox" value="Numbers" id="numbers">
+      <label for="special-chars">special characters</label>
+      <input type="checkbox" value="Special characters" id="special-chars">
+    </div>
+  </div>
+  <p>Value must be between 8 and 128</p>
+  <label for="length">Length</label>
+  <input type="number" v-model="pickedLength" id="length" name="length">
+  <p class="display-password">
+    {{ displayGeneratedPassword(pickedLength) }}
+  </p>
+</template>
+
+<style scoped>
+.display-password {
+
+}
+
+.what-to-include {
+  width: 50%;
+  margin: auto;
+}
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,11 +1,47 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+import GeneratorSection from './GeneratorSection.vue';
+
+const isGenerator = ref(false);
+
+function displayGenerator() {
+    isGenerator.value = true;
+}
 </script>
 
 <template>
 <h1>Hello !</h1>
+  <nav class="navigation">
+    <ul>
+      <li>
+        <button @click="displayIdPage">
+          Identifiants
+        </button>
+      </li>
+      <li>
+        <button @click="displayIdCreation">
+          Créer un identifiant
+        </button>
+      </li>
+      <li>
+        <button @click="displayGenerator">
+          Générer un mot de passe
+        </button>
+      </li>
+    </ul>
+  </nav>
+  <GeneratorSection v-if="isGenerator" :isGenerator=true />
 </template>
 
 <style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+}
 
+li {
+  list-style-type: none;
+}
 </style>
